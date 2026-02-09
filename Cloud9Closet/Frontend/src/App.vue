f897748cd1395800888912b941735b81600beb61
Hayden <hayden9346@hotmail.com>
2026-02-09 21:33:40 +0000

    Setup frontend and add header component

---
+++ Cloud9Closet/Frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>Cloud 9 Closet</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: Arial, sans-serif;
+        text-align: center;
+    }
+</style>
---
+++ Cloud9Closet/Frontend/vite.config.js
@@ -0,0 +1,24 @@
+﻿import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: path.resolve(__dirname, "../wwwroot/dist"),
+    emptyOutDir: true,
+    lib: {
+        entry: path.resolve(__dirname, "src/main.js"),
+        name: "App",
+        fileName: () => "main.js",
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: "main.js",
+        chunkFileNames: "[name].js",    
+        assetFileNames: "[name].[ext]",  
+      },
+    },
+    cssCodeSplit: true,
+  },
+});
